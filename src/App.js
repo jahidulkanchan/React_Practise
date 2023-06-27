b5435072088af6c0526988e040a4038d766ed9e2
jahidulkanchan <mdj524156@gmail.com>
2023-06-27 20:15:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  var heroDetails = {
+    name: "jk_morga",
+    action: "Kongfoo style",
+  };
+  var style = {
+    color: "red",
+    backgroundColor: "black",
+    padding: "25px",
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Loser life
+          Jk action style is <span style={style}>{heroDetails.action}</span>
         </p>
       </header>
     </div>
