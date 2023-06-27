c1c10aaf498f5a0957225fbc20708f6c49667272
jahidulkanchan <mdj524156@gmail.com>
2023-06-27 22:51:54 +0600

    try

--- src/App.js
+++ src/App.js
@@ -12,30 +12,63 @@ function App() {
   //   backgroundColor: "black",
   //   padding: "25px",
   // };
+
+  // // jhankar mahabub md-32-6
+  //  let pakhiList = ['Beiman','Nisthor','Pasan'];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/* jhankar mahabub md-32-4 */}
+
         {/* <p>
           Jk action style is <span style={style}>{heroDetails.action}</span>
         </p> */}
+        {/* ---------------------------- */}
         {/* jhankar mahabub md-32-5 */}
-        <Players></Players>
+
+        {/* <Players pakhiName ='Beiman' limit='kono shantona pailam na'></Players>
+        <Players pakhiName ='Boba' limit='toore r ki bolbo'></Players>
+        <Players pakhiName ='nisthor' limit='kiso hobe na tor mone'></Players> */}
+
+        {/* jhankar mahabub md-32-6 */}
+
+        {/* <Players pakhiName = {pakhiList[0]} limit='kiso hobe na tor mone'></Players>
+        <Players pakhiName = {pakhiList[1]} limit='kiso hobe na tor mone'></Players>
+        <Players pakhiName = {pakhiList[2]} limit='kiso hobe na tor mone'></Players> */}
+        <Users></Users>
+
       </header>
     </div>
   );
 }
 // jhankar mahabub md-32-5
-function Players(){
-  let styleCss ={
-    backgroundColor: '#1500e9'
+// function Players(props){
+//   let styleCss ={
+//     backgroundColor: '#1500e9',
+//     border: '2px solid white',
+//     padding: '5px 30px'
+//   }
+//   return (
+//   <div>
+//     {/* jhankar mahabub md-32-6 */}
+//     {/* <h2 style={styleCss}>Hello {props.pakhiName} Pakhi</h2>
+//     <h3 style={{backgroundColor: 'green',padding:'10px 50px'}}>{props.limit}</h3> */}
+//     <h2 style={styleCss}>Hello {props.pakhiName} Pakhi</h2>
+//     <h3 style={{backgroundColor: 'green',padding:'10px 50px'}}>{props.limit}</h3>
+//     </div>  
+//   )
+// }
+function Users(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => {
+    return(
+      <div>
+        <h2>Hello {data.name} Pakhi</h2>
+      </div>
+    )
   }
-  return (
-  <div>
-    <h2 style={styleCss}>Hello Beiman Pakhi</h2>
-    <h3 style={{backgroundColor: 'green',padding:'10px 50px'}}>Beimani nite parlam na</h3>
-    </div>  
   )
 }
 export default App;
