72ee697925031a0769f3aa2e74805908a510cf46
jahidulkanchan <mdj524156@gmail.com>
2023-06-27 21:14:39 +0600

    try

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  var heroDetails = {
-    name: "jk_morga",
-    action: "Kongfoo style",
-  };
-  var style = {
-    color: "red",
-    backgroundColor: "black",
-    padding: "25px",
-  };
+  // // jhankar mahabub md-32-4
+  // var heroDetails = {
+  //   name: "jk_morga",
+  //   action: "Kongfoo style",
+  // };
+  // var style = {
+  //   color: "red",
+  //   backgroundColor: "black",
+  //   padding: "25px",
+  // };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* jhankar mahabub md-32-4 */}
+        {/* <p>
           Jk action style is <span style={style}>{heroDetails.action}</span>
-        </p>
+        </p> */}
+        {/* jhankar mahabub md-32-5 */}
+        <Players></Players>
       </header>
     </div>
   );
 }
-
+// jhankar mahabub md-32-5
+function Players(){
+  let styleCss ={
+    backgroundColor: '#1500e9'
+  }
+  return (
+  <div>
+    <h2 style={styleCss}>Hello Beiman Pakhi</h2>
+    <h3 style={{backgroundColor: 'green',padding:'10px 50px'}}>Beimani nite parlam na</h3>
+    </div>  
+  )
+}
 export default App;
